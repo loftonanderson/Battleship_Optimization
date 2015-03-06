38e410271df33847ed28be8e759831461123c997
loftonanderson <loftonanderson@gmail.com>
2015-03-06 12:48:49 -0500

    First Push

---
+++ src/GameBoard.java
@@ -0,0 +1,603 @@
+import java.util.Random;
+		
+public class GameBoard {
+	
+	private int size = 10;
+	private int [][] boardArray = new int [size][size];
+	private int [] shipSizeArray = { 5,4,3,3,2 };
+	private String grade; 
+	// TODO add board grading (rank) system
+	
+	public GameBoard(){
+		grade = null;
+		for (int row=0; row<size; row++){
+			for (int col=0; col<size; col++){
+				boardArray[row][col] = 0;
+				//System.out.print(boardArray[row][col]);
+			}
+			//System.out.println();
+		}
+		//System.out.println();
+	}
+	
+	public GameBoard(int [][] array){
+		grade = null;
+		for (int row=0; row<size; row++){
+			for (int col=0; col<size; col++){
+				boardArray[row][col] = array[row][col];
+			}
+		}
+	}
+	
+	public int [][] getArray(){
+		return boardArray;
+	}
+	
+	public void clearBoard(){
+		for (int row=0; row<size; row++){
+			for (int col=0; col<size; col++){
+				boardArray[row][col] = 0;
+			}
+		}
+		System.out.println("------------------");
+		System.out.println("");
+	}
+	
+	public void randomlyPlace(){
+		Random generator = new Random();
+		int numOfShips = 0;
+	
+		while(numOfShips < 5){
+			int shipX = generator.nextInt(size);
+			int shipY = generator.nextInt(size);
+			
+			if (boardArray[shipY][shipX] !=0){
+				System.out.println("Random position already filled.");
+				continue;
+			}
+			else{
+				int direction = generator.nextInt(4) + 1;
+				if (direction == 1){ // North
+					boolean flag = true;
+					
+					for (int i=1; i<shipSizeArray[numOfShips]; i++){
+						if (shipY-i >= 10 || shipY-i < 0){
+							flag = false;
+						}
+						else if (boardArray[shipY-i][shipX] != 0){
+							flag = false;
+						}
+					}
+					if (flag){
+						for (int i=0; i<shipSizeArray[numOfShips]; i++){
+							boardArray[shipY-i][shipX] = shipSizeArray[numOfShips];
+						}
+						numOfShips+=1;
+					}
+				}
+				else if (direction == 2){ // East
+					boolean flag = true;
+					
+					for (int i=1; i<shipSizeArray[numOfShips]; i++){
+						if (shipX+i >= 10 || shipX+i < 0){
+							flag = false;
+						}
+						else if (boardArray[shipY][shipX+i] != 0){
+							flag = false;
+						}
+					}
+					if (flag){
+						for (int i=0; i<shipSizeArray[numOfShips]; i++){
+							boardArray[shipY][shipX+i] = shipSizeArray[numOfShips];
+						}
+						numOfShips+=1;
+					}
+				}
+				else if (direction == 3){ // South
+					boolean flag = true;
+					
+					for (int i=1; i<shipSizeArray[numOfShips]; i++){
+						if (shipY+i >= 10 || shipY+i < 0){
+							flag = false;
+						}
+						else if (boardArray[shipY+i][shipX] != 0){
+							flag = false;
+						}
+					}
+					if (flag){
+						for (int i=0; i<shipSizeArray[numOfShips]; i++){
+							boardArray[shipY+i][shipX] = shipSizeArray[numOfShips];
+						}
+						numOfShips+=1;
+					}
+				}
+				else if (direction == 4){ // West
+					boolean flag = true;
+					
+					for (int i=1; i<shipSizeArray[numOfShips]; i++){
+						if (shipX-i >= 10 || shipX-i < 0){
+							flag = false;
+						}
+						else if (boardArray[shipY][shipX-i] != 0){
+							flag = false;
+						}
+					}
+					if (flag){
+						for (int i=0; i<shipSizeArray[numOfShips]; i++){
+							boardArray[shipY][shipX-i] = shipSizeArray[numOfShips];
+						}
+						numOfShips+=1;
+					}
+				}
+			}
+		}	
+		
+		// Print out grid with ships inserted
+		/*
+		for (int row=0; row<size; row++){
+			for (int col=0; col<size; col++){
+				System.out.print(boardArray[row][col]+" ");
+			}
+			System.out.println();
+			System.out.println();
+		}
+		System.out.println();
+		*/
+	}
+	
+	public int linearSearch(){
+		int numOfHits = 0;
+		int numOfMoves = 0;
+		
+		for (int row=0; row<size; row++){
+			for (int col=0; col<size; col++){
+				if (boardArray[row][col] != 0){
+					numOfHits+=1;
+				}
+				numOfMoves+=1;
+				if (numOfHits == 17){
+					return numOfMoves;
+				}
+			}
+		}
+		return numOfMoves;
+	}
+	
+	public int randomSearch(){
+		int numOfHits = 0;
+		int numOfMoves = 0;
+		int [][] checkArray = new int [size][size];
+		Random generator = new Random();
+		
+		for (int row=0; row<size; row++){
+			for (int col=0; col<size; col++){
+				checkArray[row][col] = 0;
+			}
+		}
+		
+		while (numOfHits < 17){
+			int randCol = generator.nextInt(size);
+			int randRow = generator.nextInt(size);
+			
+			if (checkArray[randRow][randCol] != 0){
+				continue;
+			}
+			else {
+				checkArray[randRow][randCol] = 1;
+				numOfMoves+=1;
+				if (boardArray[randRow][randCol] != 0){
+					numOfHits+=1;
+				}
+			}
+		}
+		
+		return numOfMoves;
+	}
+
+	// TODO Optimize with queue and list of ships?
+	public int modifiedLinearSearch(){
+		int numOfHits = 0;
+		int numOfMoves = 0;
+		int [][] checkArray = new int [size][size];
+		
+		
+		for (int row=0; row<size; row++){
+			for (int col=0; col<size; col++){
+				checkArray[row][col] = 0;
+			}
+		}
+		
+		for (int row=0; row<size; row++){
+			for (int col=0; col<size; col++){
+				
+				// If the space has already been checked, continue to next space
+				if (checkArray[row][col] != 0){
+					continue;
+				}
+				
+				// If space hasn't been checked
+				else {
+					checkArray[row][col] = 1;
+					numOfMoves+=1;
+					
+					// If battleship is hit
+					if (boardArray[row][col] != 0){
+						numOfHits+=1;
+						if (numOfHits == 17){
+							return numOfMoves;
+						}
+						
+						// Check to the left 
+						boolean leftHit = false;
+						if (col-1 >= 0 && checkArray[row][col-1] == 0){
+							checkArray[row][col-1] = 1;
+							numOfMoves+=1;
+							if (boardArray[row][col-1] != 0){
+								numOfHits+=1;
+								leftHit = true;
+								if (numOfHits == 17){
+									return numOfMoves;
+								}
+							}
+						}
+						if (leftHit){
+							boolean keepSearching = true;
+							int num = 2;
+							while (keepSearching){
+								if (col-num >= 0 && checkArray[row][col-num] == 0){
+									checkArray[row][col-num] = 1;
+									numOfMoves+=1;
+									if (boardArray[row][col-num] != 0){
+										numOfHits+=1;
+										num+=1;
+										if (numOfHits == 17){
+											return numOfMoves;
+										}
+									}
+									else{
+										keepSearching = false;
+									}
+								}
+								else{
+									keepSearching = false;
+								}
+							}
+						}
+						if (numOfHits == 17){
+							return numOfMoves;
+						}
+						
+						// Check below
+						boolean belowHit = false;
+						if (row+1 < 10 && checkArray[row+1][col] == 0){
+							checkArray[row+1][col] = 1;
+							numOfMoves+=1;
+							if (boardArray[row+1][col] != 0){
+								numOfHits+=1;
+								belowHit = true;
+								if (numOfHits == 17){
+									return numOfMoves;
+								}
+							}
+						}
+						if (belowHit){
+							boolean keepSearching = true;
+							int num = 2;
+							while (keepSearching){
+								if (row+num < 10 && checkArray[row+num][col] == 0){
+									checkArray[row+num][col] = 1;
+									numOfMoves+=1;
+									if (boardArray[row+num][col] != 0){
+										numOfHits+=1;
+										num+=1;
+										if (numOfHits == 17){
+											return numOfMoves;
+										}
+									}
+									else{
+										keepSearching = false;
+									}
+								}
+								else{
+									keepSearching = false;
+								}
+							}
+						}
+						if (numOfHits == 17){
+							return numOfMoves;
+						}
+						
+						// Check to the right
+						boolean rightHit = false;
+						if (col+1 < 10 && checkArray[row][col+1] == 0){
+							checkArray[row][col+1] = 1;
+							numOfMoves+=1;
+							if (boardArray[row][col+1] != 0){
+								numOfHits+=1;
+								rightHit = true;
+								if (numOfHits == 17){
+									return numOfMoves;
+								}
+							}
+						}
+						if (rightHit){
+							boolean keepSearching = true;
+							int num = 2;
+							while (keepSearching){
+								if (col+num < 10 && checkArray[row][col+num] == 0){
+									checkArray[row][col+num] = 1;
+									numOfMoves+=1;
+									if (boardArray[row][col+num] != 0){
+										numOfHits+=1;
+										num+=1;
+										if (numOfHits == 17){
+											return numOfMoves;
+										}
+									}
+									else{
+										keepSearching = false;
+									}
+								}
+								else{
+									keepSearching = false;
+								}
+							}
+						}
+						if (numOfHits == 17){
+							return numOfMoves;
+						}
+						
+						// Check above
+						boolean aboveHit = false;
+						if (row-1 >= 0 && checkArray[row-1][col] == 0){
+							checkArray[row-1][col] = 1;
+							numOfMoves+=1;
+							if (boardArray[row-1][col] != 0){
+								numOfHits+=1;
+								aboveHit = true;
+								if (numOfHits == 17){
+									return numOfMoves;
+								}
+							}
+						}
+						if (aboveHit){
+							boolean keepSearching = true;
+							int num = 2;
+							while (keepSearching){				
+								if (row-num >= 0 && checkArray[row-num][col] == 0){
+									checkArray[row-num][col] = 1;
+									numOfMoves+=1;
+									if (boardArray[row-num][col] != 0){
+										numOfHits+=1;
+										num+=1;
+										if (numOfHits == 17){
+											return numOfMoves;
+										}
+									}
+									else{
+										keepSearching = false;
+									}
+								}
+								else{
+									keepSearching = false;
+								}
+							}
+						}
+						if (numOfHits == 17){
+							return numOfMoves;
+						}
+					}
+					
+					// If all battleships are sunk, return number of moves
+					if (numOfHits == 17){
+						return numOfMoves;
+					}
+				}
+			}
+		}	
+		return numOfMoves;
+	}
+	
+	public int modifiedRandomSearch(){
+		int numOfHits = 0;
+		int numOfMoves = 0;
+		int [][] checkArray = new int [size][size];
+		Random generator = new Random();
+		
+		for (int row=0; row<size; row++){
+			for (int col=0; col<size; col++){
+				checkArray[row][col] = 0;
+			}
+		}
+	
+		while (numOfHits < 17){
+			int col = generator.nextInt(size);
+			int row = generator.nextInt(size);
+			
+			// If the space has already been checked, continue to next space
+			if (checkArray[row][col] != 0){
+				continue;
+			}
+			
+			// If space hasn't been checked
+			else {
+				checkArray[row][col] = 1;
+				numOfMoves+=1;
+				
+				// If battleship is hit
+				if (boardArray[row][col] != 0){
+					numOfHits+=1;
+					if (numOfHits == 17){
+						return numOfMoves;
+					}
+					
+					// Check to the left 
+					boolean leftHit = false;
+					if (col-1 >= 0 && checkArray[row][col-1] == 0){
+						checkArray[row][col-1] = 1;
+						numOfMoves+=1;
+						if (boardArray[row][col-1] != 0){
+							numOfHits+=1;
+							leftHit = true;
+							if (numOfHits == 17){
+								return numOfMoves;
+							}
+						}
+					}
+					if (leftHit){
+						boolean keepSearching = true;
+						int num = 2;
+						while (keepSearching){
+							if (col-num >= 0 && checkArray[row][col-num] == 0){
+								checkArray[row][col-num] = 1;
+								numOfMoves+=1;
+								if (boardArray[row][col-num] != 0){
+									numOfHits+=1;
+									num+=1;
+									if (numOfHits == 17){
+										return numOfMoves;
+									}
+								}
+								else{
+									keepSearching = false;
+								}
+							}
+							else{
+								keepSearching = false;
+							}
+						}
+					}
+					if (numOfHits == 17){
+						return numOfMoves;
+					}
+					
+					// Check below
+					boolean belowHit = false;
+					if (row+1 < 10 && checkArray[row+1][col] == 0){
+						checkArray[row+1][col] = 1;
+						numOfMoves+=1;
+						if (boardArray[row+1][col] != 0){
+							numOfHits+=1;
+							belowHit = true;
+							if (numOfHits == 17){
+								return numOfMoves;
+							}
+						}
+					}
+					if (belowHit){
+						boolean keepSearching = true;
+						int num = 2;
+						while (keepSearching){
+							if (row+num < 10 && checkArray[row+num][col] == 0){
+								checkArray[row+num][col] = 1;
+								numOfMoves+=1;
+								if (boardArray[row+num][col] != 0){
+									numOfHits+=1;
+									num+=1;
+									if (numOfHits == 17){
+										return numOfMoves;
+									}
+								}
+								else{
+									keepSearching = false;
+								}
+							}
+							else{
+								keepSearching = false;
+							}
+						}
+					}
+					if (numOfHits == 17){
+						return numOfMoves;
+					}
+					
+					// Check to the right
+					boolean rightHit = false;
+					if (col+1 < 10 && checkArray[row][col+1] == 0){
+						checkArray[row][col+1] = 1;
+						numOfMoves+=1;
+						if (boardArray[row][col+1] != 0){
+							numOfHits+=1;
+							rightHit = true;
+							if (numOfHits == 17){
+								return numOfMoves;
+							}
+						}
+					}
+					if (rightHit){
+						boolean keepSearching = true;
+						int num = 2;
+						while (keepSearching){
+							if (col+num < 10 && checkArray[row][col+num] == 0){
+								checkArray[row][col+num] = 1;
+								numOfMoves+=1;
+								if (boardArray[row][col+num] != 0){
+									numOfHits+=1;
+									num+=1;
+									if (numOfHits == 17){
+										return numOfMoves;
+									}
+								}
+								else{
+									keepSearching = false;
+								}
+							}
+							else{
+								keepSearching = false;
+							}
+						}
+					}
+					if (numOfHits == 17){
+						return numOfMoves;
+					}
+					
+					// Check above
+					boolean aboveHit = false;
+					if (row-1 >= 0 && checkArray[row-1][col] == 0){
+						checkArray[row-1][col] = 1;
+						numOfMoves+=1;
+						if (boardArray[row-1][col] != 0){
+							numOfHits+=1;
+							aboveHit = true;
+							if (numOfHits == 17){
+								return numOfMoves;
+							}
+						}
+					}
+					if (aboveHit){
+						boolean keepSearching = true;
+						int num = 2;
+						while (keepSearching){				
+							if (row-num >= 0 && checkArray[row-num][col] == 0){
+								checkArray[row-num][col] = 1;
+								numOfMoves+=1;
+								if (boardArray[row-num][col] != 0){
+									numOfHits+=1;
+									num+=1;
+									if (numOfHits == 17){
+										return numOfMoves;
+									}
+								}
+								else{
+									keepSearching = false;
+								}
+							}
+							else{
+								keepSearching = false;
+							}
+						}
+					}
+					if (numOfHits == 17){
+						return numOfMoves;
+					}
+				}
+				
+				// If all battleships are sunk, return number of moves
+				if (numOfHits == 17){
+					return numOfMoves;
+				}
+			}
+		}
+		return numOfMoves;
+	}
+}
---
+++ src/Main.java
@@ -0,0 +1,150 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+
+		boolean generate = false; // If false test values, if true generate "numOfBoards" boards
+		int numOfBoards = 500; // Change this to increase number of boards generated.
+		int size = 10; // 10x10 board
+		GameBoard [] masterList = new GameBoard [numOfBoards];
+		GameBoard [] inputList = new GameBoard [numOfBoards];
+		int [] linearSearchList = new int [numOfBoards];
+		int [] randomSearchList = new int [numOfBoards];
+		int [] modifiedLinearSearchList = new int [numOfBoards];
+		int [] modifiedRandomSearchList = new int [numOfBoards];
+		Scanner x = null;
+		double linearAvg = 0;
+		double randomAvg = 0;
+		double modifiedLinearAvg = 0;
+		double modifiedRandomAvg = 0;
+		
+		
+		
+		if (!generate){
+			// Used to read Boards from file
+			try{
+				x = new Scanner(new File ("GameBoards"));
+			}
+			catch(Exception e){
+				System.out.println("could not find file");
+			}
+			
+			for (int i=0; i<numOfBoards; i++){
+				int [][] boardArray = new int [size][size];
+				int row = 0;
+				int col = 0;
+			
+				while(row<11){
+	            String line = x.nextLine();
+		            for (int j = 0; j < line.length(); j++) {
+		            	int peg = (int)(line.charAt(j)) - 48;
+		            	boardArray[row][col] = peg;
+		            	col++;
+		            }
+	            row++;
+	            col = 0;
+				}
+				GameBoard GB = new GameBoard(boardArray);
+				inputList[i] = GB;
+			}
+			
+			x.close();
+			
+			for (int i=0; i<numOfBoards; i++){
+				// Erase/comment out nested for loop bellow to erase boards being printed out
+				for (int row=0; row<size; row++){
+					for (int col=0; col<size; col++){
+						System.out.print(inputList[i].getArray()[row][col]);
+					}
+					System.out.println();
+				}
+				// Erase/comment out every print statement to only calculate data 
+				// and not have every board's search output printed out
+				System.out.println();
+				linearSearchList[i] = inputList[i].linearSearch();
+				System.out.println("Linear Search: " +inputList[i].linearSearch());
+				randomSearchList[i] = inputList[i].randomSearch();
+				System.out.println("Random Search: " +inputList[i].randomSearch());
+				modifiedLinearSearchList[i] = inputList[i].modifiedLinearSearch();
+				System.out.println("Modified Linear Search: " +inputList[i].modifiedLinearSearch());
+				modifiedRandomSearchList[i] = inputList[i].modifiedRandomSearch();
+				System.out.println("Modified Random Search: " + inputList[i].modifiedRandomSearch());
+				System.out.println();
+			}
+			
+			System.out.println("##########");
+			System.out.println("## Data ##");
+			System.out.println("##########");
+			System.out.println();
+
+			for (int i=0; i<numOfBoards; i++){
+				System.out.print(linearSearchList[i] +" ");
+				linearAvg += linearSearchList[i];
+			}
+			System.out.println();
+			for (int i=0; i<numOfBoards; i++){
+				System.out.print(randomSearchList[i] +" ");
+				randomAvg += randomSearchList[i];
+			}
+			System.out.println();
+			for (int i=0; i<numOfBoards; i++){
+				System.out.print(modifiedLinearSearchList[i] +" ");
+				modifiedLinearAvg += modifiedLinearSearchList[i];
+			}
+			System.out.println();
+			for (int i=0; i<numOfBoards; i++){
+				System.out.print(modifiedRandomSearchList[i] +" ");
+				modifiedRandomAvg += modifiedRandomSearchList[i];
+			}
+			
+			linearAvg = linearAvg/numOfBoards;
+			randomAvg = randomAvg/numOfBoards;
+			modifiedLinearAvg = modifiedLinearAvg/numOfBoards;
+			modifiedRandomAvg = modifiedRandomAvg/numOfBoards;
+			System.out.println();
+			System.out.println();
+			System.out.println("Linear Average: " + linearAvg);
+			System.out.println("Random Average: " + randomAvg);
+			System.out.println("Modified Linear Average: " + modifiedLinearAvg);
+			System.out.println("Modified Random Average: " + modifiedRandomAvg);
+		}
+		
+// ---------------------------------------------------------------------------------		
+		
+		else {
+			
+			// Generate Boards
+			
+			for (int i=0; i< numOfBoards; i++){
+				GameBoard GB = new GameBoard();
+				GB.randomlyPlace();
+				masterList[i] = GB;
+			}
+		
+			// print Boards to file
+			
+			try {
+				FileWriter fw = new FileWriter("Gameboards");
+				PrintWriter pw = new PrintWriter(fw);
+				
+				// Probably not the most efficient way
+				for (int i=0; i<numOfBoards; i++){
+					for (int row=0; row<size; row++){
+						for (int col=0; col<size; col++){
+							pw.print(masterList[i].getArray()[row][col]);
+						}
+					pw.println();
+					}
+				pw.println();
+				}
+				pw.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
